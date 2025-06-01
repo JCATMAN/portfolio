d208380173ae9f45157362ecc7e757f6261d390b
Jose Hernandez <jose.hernandezhoyos@gmail.com>
2025-06-01 12:16:27 -0500

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import image from '@astrojs/image';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [image()]
+});
